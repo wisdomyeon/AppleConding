5153689664430c07852fd627e98b668ef5a36615
Yeonji Choi <duswl8936@naver.com>
2022-11-23 21:59:41 +0900

    221123 Effect 숙제

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,93 +1,102 @@
-import { useState } from 'react';
-import './App.css';
-import {Button, Navbar, Container, Nav} from 'react-bootstrap';
-import data from './component/data.js';
-import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom"
-import Detail from './page/Detail.jsx';
-import styled from 'styled-components'
+import { useState } from "react";
+import "./App.css";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import data from "./component/data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./page/Detail.jsx";
+import styled from "styled-components";
 
 let Btn = styled.button`
-  color : pink;
-  background : white;
-  border : 1px solid black;
-`
+  color: pink;
+  background: white;
+  border: 1px solid black;
+`;
 
 function App() {
-  let [shoesdata, setShoesData] = useState(data)
+  let [shoesdata, setShoesData] = useState(data);
   let navigate = useNavigate();
 
   return (
     <div className="App">
-      
-       <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Yeon's</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home" onClick={()=>navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/detail')}>Detail</Nav.Link>
-            <Nav.Link onClick={() => navigate('/about')}>About</Nav.Link> 
+            <Nav.Link href="#home" onClick={() => navigate("/")}>
+              Home
+            </Nav.Link>
+            <Nav.Link onClick={() => navigate("/detail")}>Detail</Nav.Link>
+            <Nav.Link onClick={() => navigate("/about")}>About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      
+
       <Routes>
-        <Route path='/' element={ 
-           <>
-           <div className='main-bg'></div>
-           <div className='container'>
-             <div className='row'>
-               {
-                 shoesdata.map((shoes)=>{ 
-                   return <ShoesData shoesdata={shoes} key={shoes.id}></ShoesData>
-                 })
-               }
-             </div>
-             <Button variant="primary">Primary</Button>
-             <Button variant="light" onClick={() => {
-                let temp = [...shoesdata];
-                temp.sort((a, b) => {
-                if (a.title < b.title) {
-                  return -1;
-                }
-                else if (a.title > b.title) {
-                  return 1;
-                }
-                });
-                setShoesData(temp);
-              }}>가나다순 정렬</Button>
-             <Btn>버튼</Btn>
-           </div>
-           </>
-         } />
-         
-        <Route path='/detail/:id' element={<Detail shoesdata={shoesdata}/>}/>
-        <Route path='*' element={<div>404 페이지</div>}/>
-    
-        <Route path='/about' element={<About/>}>
-          <Route path='member' element={<div>멤버 정보</div>}/>
-          <Route path='location' element={<div>장소 정보</div>}/>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoesdata.map((shoes) => {
+                    return (
+                      <ShoesData shoesdata={shoes} key={shoes.id}></ShoesData>
+                    );
+                  })}
+                </div>
+                <Button variant="primary">Primary</Button>
+                <Button
+                  variant="light"
+                  onClick={() => {
+                    let temp = [...shoesdata];
+                    temp.sort((a, b) => {
+                      if (a.title < b.title) {
+                        return -1;
+                      } else if (a.title > b.title) {
+                        return 1;
+                      }
+                    });
+                    setShoesData(temp);
+                  }}
+                >
+                  가나다순 정렬
+                </Button>
+                <Btn>버튼</Btn>
+              </div>
+            </>
+          }
+        />
+
+        <Route path="/detail/:id" element={<Detail shoesdata={shoesdata} />} />
+        <Route path="*" element={<div>404 페이지</div>} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버 정보</div>} />
+          <Route path="location" element={<div>장소 정보</div>} />
         </Route>
 
-        < Route path='/event' element={ <Event/>}>
-          <Route path='one' element={<div> 첫 주문시 양배추즙 서비스 </div>} />
-          <Route path='two' element={<div> 생일기념 쿠폰받기 </div>} />
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div> 첫 주문시 양배추즙 서비스 </div>} />
+          <Route path="two" element={<div> 생일기념 쿠폰받기 </div>} />
         </Route>
-      </Routes> 
-
-      
+      </Routes>
     </div>
   );
 }
 export default App;
 
 function ShoesData(props) {
-  return(
-    <div className='col-md-4'>
-      <img src={process.env.PUBLIC_URL + `/shoes${props.shoesdata.id+1}.jpg`} width="80%"></img>
+  return (
+    <div className="col-md-4">
+      <img
+        src={process.env.PUBLIC_URL + `/shoes${props.shoesdata.id + 1}.jpg`}
+        width="80%"
+      ></img>
       <h4>{props.shoesdata.title}</h4>
       <p>{props.shoesdata.price}</p>
     </div>
-  )
+  );
 }
 
 function About() {
@@ -96,7 +105,7 @@ function About() {
       <h4>회사정보</h4>
       <Outlet></Outlet>
     </div>
-  )
+  );
 }
 
 function Event() {
@@ -105,5 +114,5 @@ function Event() {
       <h3>오늘의 이벤트</h3>
       <Outlet></Outlet>
     </div>
-  )
+  );
 }
--- shop/src/page/Detail.jsx
+++ shop/src/page/Detail.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Detail = (props) => {
-    let {id} = useParams();
+  let { id } = useParams();
+  let [event, setEvent] = useState(true);
 
-    return (
-    <div className='container'>
-        <div className='row'>
-            <div className='col-md-6'>
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />    
-            </div>
-            <div className='col-md-6'>
-                <h4 className='pt-5'>{props.shoesdata[id].title}</h4>
-                <p>{props.shoesdata[id].content}</p>
-                <p>{props.shoesdata[id].price}</p>
-                <button className='btn btn-danger'>주문하기</button>
-            </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setEvent(false);
+    }, 2000);
+    return () => {
+      console.log("디테일 컴포넌트 삭제");
+    };
+  });
+
+  return (
+    <div className="container">
+      {event ? (
+        <div className="alert alert-warning"> 2초이내 구매시 할인</div>
+      ) : null}
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src={
+              process.env.PUBLIC_URL + `/shoes${props.shoesdata[id].id + 1}.jpg`
+            }
+            width="100%"
+          />
         </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{props.shoesdata[id].title}</h4>
+          <p>{props.shoesdata[id].content}</p>
+          <p>{props.shoesdata[id].price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Detail;
-
-
-
-
