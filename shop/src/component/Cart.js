0ca7cbf672423c92c20f363cdd41b59f10252de3
Yeonji Choi <duswl8936@naver.com>
2023-01-07 18:59:36 +0900

    Redux4/숙제완료

--- shop/src/component/Cart.js
+++ shop/src/component/Cart.js
@@ -1,42 +1,48 @@
 import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { plusCount } from "./../store";
+import { plusCount, aa } from "./../store";
 
 function Cart() {
-    let allState = useSelector((state) => { return state })
-    let dispatch = useDispatch()
+  let allState = useSelector((state) => {
+    return state;
+  });
+  let dispatch = useDispatch();
 
-    //console.log(allState.user);
-    //console.log(allState.inventory);
-    //console.log(allState.items[0]);
-    //또는 let user = useSelector((state)=>{ return state.user}) 식으로 사용가능
-    // console.log(user);
+  //console.log(allState.user);
+  //console.log(allState.inventory);
+  //console.log(allState.items[0]);
+  //또는 let user = useSelector((state)=>{ return state.user}) 식으로 사용가능
+  // console.log(user);
 
-    return (
-        <Table striped>
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>상품명</th>
-                    <th>수량</th>
-                    <th>변경하기</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    allState.items.map((cur, index) =>
-                        <tr>
-                            <td>{allState.items[index].id + 1}</td>
-                            <td>{allState.items[index].name}</td>
-                            <td>{allState.items[index].count}</td>
-                            <td><button onClick={() => {
-                                dispatch(plusCount())
-                            }}>+</button></td>
-                        </tr>
-                    )
-                }
-            </tbody >
-        </Table >
-    )
+  return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>상품명</th>
+          <th>수량</th>
+          <th>변경하기</th>
+        </tr>
+      </thead>
+      <tbody>
+        {allState.items.map((cur, index) => (
+          <tr>
+            <td>{allState.items[index].id + 1}</td>
+            <td>{allState.items[index].name}</td>
+            <td>{allState.items[index].count}</td>
+            <td>
+              <button
+                onClick={() => {
+                  dispatch(plusCount({ action: cur.id }));
+                }}
+              >
+                +
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 }
 export default Cart;
